367b90fff6345d5d180f043f0d15d9e2e7316f1e
mmmm_lemon <egorproger@gmail.com>
2020-11-02 16:11:00 +1000

    Старт

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Statify</title>
+</head>
+<body>
+    <h1>Statify</h1>
+    <h4>A Laravel/Vue.js/Spotify Web API application</h5>
+    <hr>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1024px-Laravel.svg.png" width="100px" alt="">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png" width="100px" alt="">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" width="100px" alt="">
+    <hr>
+    <p>blah blah blah some content here</p>
+</body>
+</html>
