dc774a8ff5680d1ec9af99a6b540f81488e1eec2
mmmm_lemon <egorproger@gmail.com>
2020-11-07 15:47:26 +1000

    Добавлен Honeymoon
    Honeymoon - CSS/HTML фреймворк

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,74 +1,122 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{-- HEAD --}}
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    {{-- Заголовок веб-сайта --}}
     <title>{{ config('settings')->site_title }}</title>
 
-    <!-- Scripts -->
+    <!-- JS Скрипты -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/halfmoon.min.js') }}" defer></script>
 
-    <!-- Fonts -->
+    <!-- Шрифты -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- CSS Стили -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/halfmoon-variables.css') }}" rel="stylesheet">
+
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <img src="/logo.png" width="50px" style="margin-right: 10px;" alt="">
-                <router-link to="/">
-                    <a class="navbar-brand" style="font-size:20pt; color: #0077B9;">
-                        {{ config('settings')->site_title }}
-                    </a>
-                </router-link>
+
+{{-- BODY --}}
+<body class="dark-mode">
+
+    <div class="container-sm">
+
+        <div id="app" class="page-wrapper with-navbar">
+
+            <nav class="navbar">
             
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li style="margin-right: 10px;">
-                            <router-link to="/" class="nav_item"><i class="fa fa-dashboard"></i>Главная</router-link>
-                        </li>
-                        <li style="margin-right: 10px;">
-                            <router-link to="/tests" class="nav_item"><i class="fa fa-dashboard"></i>Dev: Tests</router-link>
-                        </li>
-                    </ul>
-
-                    <!-- Правая сторона навигации -->
-                    <ul class="navbar-nav ml-auto">
-                        {{-- Авторизация через Spotify --}}
-                        @if($access_check == false)
-                            <li style="margin-right: 10px;">
-                                <a href="/spotify_auth" class="nav_item_login_spotify">Войти в Spotify</a>
-                            </li>
-                        @else
-                            {{-- юзернейм --}}
-                            <router-link to="/spotify_profile" class="nav_item_username">{{$spotify_profile['display_name']}}</a></router-link>
-                            {{-- юзерпик --}}
-                            <img src="{{$spotify_profile['avatar']}}" alt="Spotify avatar" class="nav_item_spotify_avatar rounded-circle">
-                            {{-- кнопка выхода --}}
-                            <a href="/spotify_logout" class="nav_item_logout">Выйти</a>
-                        @endif
-                    </ul>
-                </div>
+            <div class="navbar-content">
+                <img src="/logo.png" width="50px" style="margin-right:10px; padding:5px;" alt="">
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+    
+            <router-link to="/"><a class="navbar-brand">
+                {{config('settings')->site_title}}
+            </a></router-link>
+    
+            <span class="navbar-text text-monospace">v0.0</span>
+    
+            <ul class="navbar-nav d-none d-md-flex">
+                <li class="nav-item active">
+                    <router-link to="/"><a class="nav-link">Главная</a></router-link>
+                </li>
+                <li class="nav-item active">
+                    <router-link to="/tests"><a class="nav-link">Dev: Tests</a></router-link>
+                </li>
+            </ul>
+            @if($access_check == false)
+                <div class="form-inline d-none d-md-flex ml-auto"> 
+                    <a href="/spotify_auth" class="btn btn-primary btn-rounded">Войти в Spotify</a>
+                </div>
+            @else
+                <div class="form-inline d-none d-md-flex ml-auto" style="margin-right:3%;"> 
+                        {{-- юзернейм --}}
+                        <router-link to="/spotify_profile" class=""><a class="nav-link">{{$spotify_profile['display_name']}}</a></router-link>
+                        {{-- юзерпик --}}
+                        <img src="{{$spotify_profile['avatar']}}" alt="Spotify avatar" class="nav_item_spotify_avatar rounded-circle">
+                        {{-- кнопка выхода --}}
+                        <a class="nav-link" href="/spotify_logout" class="">Выйти</a>
+                </div>
+            @endif
+            
+            {{-- Меню для мобилок --}}
+            <div class="navbar-content d-md-none ml-auto"> 
+    
+                <div class="dropdown with-arrow">
+                
+                <button class="btn" data-toggle="dropdown" type="button" id="navbar-dropdown-toggle-btn-1">
+                    Меню
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                </button>
+    
+                <div class="dropdown-menu dropdown-menu-right w-200" aria-labelledby="navbar-dropdown-toggle-btn-1">
+                    <router-link to="/"><a class="dropdown-item">Главная</a></router-link>
+                    <router-link to="/tests"><a class="dropdown-item">Dev:Tests</a></router-link>
+                    <div class="dropdown-divider"></div>
+                    
+                    @if($access_check == false)
+                    <div class="dropdown-content">
+                        <a href="/spotify_auth" class="btn btn-primary btn-block btn-rounded">Войти в Spotify</a>
+                    </div>    
+                    @else
+                    <div class="dropdown-content">
+                        {{-- юзернейм --}}
+                        <router-link to="/spotify_profile" class="">{{$spotify_profile['display_name']}}</a></router-link>
+                        {{-- кнопка выхода --}}
+                        <a href="/spotify_logout" class="" style="margin-left:20px;">X</a>
+                    </div>  
+                    @endif
+                </div>
+                
+                </div>
+    
+            </div>
+    
+            </nav>
+        
+            <!-- Content wrapper -->
+            <div class="content-wrapper" style="padding-left:2%; padding-right:2%;">
+            <div class="container" >
+                <main class="py-4">
+                    @yield('content')
+                </main>
+            </div>
+            </div>
+    
+        </div>
     </div>
+
+
 </body>
+
 </html>
